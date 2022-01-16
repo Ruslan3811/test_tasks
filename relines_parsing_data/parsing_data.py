4d7099b5e29a4d4c1fb0c18c8d2bb240eb5ae432
unknown <rmyagdeev@mail.ru>
2022-01-16 16:33:19 +0300

    added parsing data from url

--- relines_parsing_data/parsing_data.py
+++ relines_parsing_data/parsing_data.py
@@ -1,4 +1,6 @@
 import argparse
+import requests
+from bs4 import BeautifulSoup
 
 
 def parse_cmd_args():
@@ -11,6 +13,17 @@ def parse_cmd_args():
     args = parser.parse_args()
     return args
 
+def parsing_site(parsed_args):
+    """Getting data from url"""
+    url = parsed_args.url
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        return response, soup
+    except:
+        print("Error: 404 Not Found")
+        exit()
 
 if __name__ == "__main__":
     parsed_args = parse_cmd_args()
+    response, soup = parsing_site(parsed_args)
